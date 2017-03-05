19994a39211d5cee302b27c3d9217929c45f7951
ranyand <root@localhost.localdomain>
2017-03-05 18:01:04 +0800

    html js py

---
+++ js/myScript1.js
@@ -0,0 +1,14 @@
+/**
+ * Single Page Nav Plugin
+ * Copyright (c) 2013 Chris Wojcik <hello@chriswojcik.net>
+ * Dual licensed under MIT and GPL.
+ * @author Chris Wojcik
+ * @version 1.1.0
+ */
+
+// Utility
+function myFunction()
+{
+  document.getElementById("demo")innerHTML="now it changed";
+
+}
